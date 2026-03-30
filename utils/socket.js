9a5bc541670e8ff31f4526e11dfb90034e982db1
sheikhhaad <sheikhhaad6@gmail.com>
2026-03-30 18:18:19 +0500

    d

--- utils/socket.js
+++ utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8000", {
+const socket = io("https://stu-portal-backend.vercel.app", {
   transports: ["websocket", "polling"],
 });
 
