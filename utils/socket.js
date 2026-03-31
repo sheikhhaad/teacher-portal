308543756e212b599f4164fc327eb650c35d9b86
sheikhhaad <sheikhhaad6@gmail.com>
2026-03-31 17:31:40 +0500

    .

--- utils/socket.js
+++ utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8000");
+const socket = io("https://stu-portal-backend.vercel.app");
 
 socket.on("connect", () => console.log("Socket connected:", socket.id));
 socket.on("connect_error", (err) => console.log("Socket error:", err));
