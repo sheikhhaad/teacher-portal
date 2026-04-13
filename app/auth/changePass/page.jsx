80494e7985278301e0313165a070fb6c2a45c423
sheikhhaad <sheikhhaad6@gmail.com>
2026-04-13 15:10:59 +0500

    .

--- app/auth/changePass/page.jsx
+++ app/auth/changePass/page.jsx
@@ -1,6 +1,6 @@
 // app/reset-password/page.js
 "use client";
-
+export const dynamic = "force-dynamic";
 import api from "@/utils/api";
 import React, { useState, useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
