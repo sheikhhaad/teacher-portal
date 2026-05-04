d55fe31e5bd52073f057837a7711bd3a186849f0
sheikhhaad <sheikhhaad6@gmail.com>
2026-05-04 20:29:29 +0500

    feat: initialize axios instance and socket.io client for backend communication

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL = "https://stu-portal-backend.vercel.app";
-// "http://localhost:8000";
+//  "http://localhost:8000";
 
 const api = axios.create({
   baseURL: API_URL,
--- utils/socket.js
+++ utils/socket.js
@@ -1,5 +1,10 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://stu-portal-backend.vercel.app", {
-  withCredentials: true,
-});
+export const socket = io(
+  "https://stu-portal-backend.vercel.app",
+  // "http://localhost:8000",
+  {
+    transports: ["websocket"],
+    withCredentials: true,
+  },
+);
